e81c918160d0e1adf43597dcb953e9103f08e123
praptimane26 <praptimane26@github.com>
2021-10-18 00:53:25 +1300

    Pagination works fine only set at the bottom of the page as the top didn't look nice

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import { useEffect, useState, useCallback } from "react"; //useCallback used to 
 //it remembers the data but doesn't update it unless we need it to
 import query from "./Query";
 import RepoInfo from "./RepoInfo";
+import SearchBox from "./SearchBox.js";
+import NavButtons from "./NavButtons.js";
 
 function App() {
   let [userName, setUserName] = useState("");
   let [repoList, setRepoList] = useState(null);
   let [pageCount, setPageCount] = useState(10);
-  let [queryString, setQueryString] = useState("website");
+  let [queryString, setQueryString] = useState("");
   let [totalCount, setTotalCount] = useState(null);
 
+  let [startCursor, setStartCursor] = useState(null);
+  let [endCursor, setEndCursor] = useState(null);
+  let [hasPreviousPage, setHasPreviousPage] = useState(false);
+  let [hasNextPage, setHasNextPage] = useState(true);
+  let [paginationKeyword, setPaginationKeyword] = useState("first");
+  let [paginationString, setPaginationString] = useState("");
+
   const fetchData = useCallback(() => {
-    const queryText = JSON.stringify(query(pageCount, queryString));
+    const queryText = JSON.stringify(
+      query(pageCount, queryString, paginationKeyword, paginationString)
+    );
 
     fetch(github.baseURL, {
       method: "POST",
@@ -22,16 +33,26 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         const viewer = data.data.viewer;
-        const repos = data.data.search.nodes;
+        const repos = data.data.search.edges;
         const total = data.data.search.repositoryCount;
+        const start = data.data.search.pageInfo?.startCursor;
+        const end = data.data.search.pageInfo?.endCursor;
+        const next = data.data.search.pageInfo?.hasNextPage;
+        const prev = data.data.search.pageInfo?.hasPreviousPage;
+
         setUserName(viewer.name);
         setRepoList(repos);
         setTotalCount(total);
+
+        setStartCursor(start);
+        setEndCursor(end);
+        setHasNextPage(next);
+        setHasPreviousPage(prev);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [pageCount, queryString]);
+  }, [pageCount, queryString, paginationString, paginationKeyword]);
 
   useEffect(() => {
     fetchData();
@@ -42,17 +63,36 @@ function App() {
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
       <p>Hey there {userName}</p>
-      <p>
-        <b>Search for:</b> {queryString} | <b>Items per page:</b> {pageCount} |
-        <b>Total results:</b> {totalCount}
-      </p>
+      <SearchBox
+        totalCount={totalCount}
+        pageCount={pageCount}
+        queryString={queryString}
+        onTotalChange={(myNumber) => {
+          setPageCount(myNumber);
+        }}
+        onQueryChange={(myString) => {
+          setQueryString(myString);
+        }}
+      />
+
       {repoList && (
         <ul className="list-group list-group-flush">
           {repoList.map((repo) => (
-            <RepoInfo key={repo.id} repo={repo} />
+            <RepoInfo key={repo.node.id} repo={repo.node} />
           ))}
         </ul>
       )}
+
+      <NavButtons
+        start={startCursor}
+        end={endCursor}
+        next={hasNextPage}
+        previous={hasPreviousPage}
+        onPage={(myKeyword, myString) => {
+          setPaginationKeyword(myKeyword);
+          setPaginationString(myString);
+        }}
+      />
     </div>
   );
 }
