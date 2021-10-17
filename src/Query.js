e8e05bc37766f0783bc0ceb2184e0994231dd875
praptimane26 <praptimane26@github.com>
2021-10-18 00:09:43 +1300

    search working fine

--- src/Query.js
+++ src/Query.js
@@ -1,26 +1,29 @@
-const githubQuery = {
-  query: `
-  {
-    viewer {
-      name
-    }
-    search(query: "user:praptimane26 sort:updated-desc", type: REPOSITORY, first: 20) {
-      nodes {
-        ... on Repository {
-          name
-          description
-          id
-          url
-          viewerSubscription
-          licenseInfo {
-            spdxId
-          }
+const githubQuery = (pageCount, queryString) => {
+  return {
+    query: `
+            {
+                viewer {
+                    name
+                }
+                search(query: "${queryString} user:praptimane26 sort:updated-desc", type: REPOSITORY, first: ${pageCount}) {
+                    repositoryCount
+                    nodes {
+                    ... on Repository {
+                        name
+                        description
+                        id
+                        url
+                        viewerSubscription
+                        licenseInfo {
+                        spdxId
+                        }
+                    }
+                }
+            }
         }
-      }
-    }
-  }
-    
-  `,
+      
+    `,
+  };
 };
 
 export default githubQuery;
