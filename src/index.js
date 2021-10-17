1772ce5b41ef2d0cd76e3c8645cbc7378c7bd760
praptimane26 <praptimane26@github.com>
2021-10-17 16:07:44 +1300

    created project, installed bootstrap modules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* This is the main file this is the entry point for the application, the index.html file will read this index.js file 
+and load the entire application. This file imports different code from different librabries*/
+import React from "react"; //main library for react
+import ReactDOM from "react-dom"; //used to control the document object model(structure of how web pages are put together)
+import App from "./App"; //when importing modules you don't need to add .js extention
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
