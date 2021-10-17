4cb9396f682a6f5b9b4ee8e6230c1961cb405e5f
praptimane26 <praptimane26@github.com>
2021-10-17 19:44:31 +1300

    created db.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import github from "./db.js";
+import { useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i> Repos
+      </h1>
+    </div>
+  );
 }
 
 export default App;
